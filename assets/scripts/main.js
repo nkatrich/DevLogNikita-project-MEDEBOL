162e2ae392c031297cfe9fa484bafddd19da1466
Nikita <nikitakatrich.kzn@gmail.com>
2026-01-15 20:19:16 +0300

    added logic make short text, using function programming with argues, as well, have done with 1 dialog logic, modal offer, to check whole info about offer, one more thing that done, delegation events. For 15 cards of offer, modern method of code

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,9 +11,16 @@
     // modal windows
     const dialog = document.querySelector('dialog');
     const modalOffer = document.querySelector('.modal-offer');
+        // modal offer tags
+        const contentImageOffer = document.querySelector('.content-image-offer img');
+        const nameDescriptionOfOffer = document.querySelector('.name-description-of-offer');
+        const price = document.querySelector('.price');
+        const throwInBin = document.querySelector('.throw-in-bin');
     const bagOfOffers = document.querySelector('.bag-of-offers');
     const formForBuy = document.querySelector('.form-for-buy');
     const btnOrderPreform = document.querySelector('.div-order-preform button');
+    const suggestedOffers = document.querySelector('.suggested-offers');
+    const pSuggestedOffer = document.querySelector('.p-suggested-offer');
 
     // modal window form
     const order = document.querySelector('.order');
@@ -23,6 +30,7 @@
     const currentBag = document.querySelector('.current-bag');
 
     // cards-content
+    const divOffers = document.querySelector('.div-offers');
     const cardOfferImg = document.querySelectorAll('.card-offer img');
     const descriptionOfOffer = document.querySelectorAll('.description-of-offer');
     const currentGoods = document.querySelector('.current-goods');
@@ -100,37 +108,62 @@ window.addEventListener('scroll', () => {
 // entire list is in order with HTML structure
 const contentCard = [
     // sofas
-    {src: './assets/images/furniture/furniture-1.webp', text: 'Современный трехместный диван с прямыми линиями. Имеет низкую спинку и тонкие высокие ножки, что придает конструкции легкость. Выполнен в лаконичном скандинавском стиле.', id: 1, cardAN: "#card-a-1"},
-    {src: './assets/images/furniture/furniture-2.webp', text: 'Уютный диван песочного или светло-коричневого оттенка. Отличается пышными формами, мягкими подлокотниками и дополнительными подушками спинки. Идеально подойдет для традиционного интерьера.', id: 2, cardAN: "#card-a-2"},
-    {src: './assets/images/furniture/furniture-3.webp', text: 'Стильный диван графитового цвета на металлическом каркасе. Дизайн подчеркнут четкими формами подушек и видимым основанием, что характерно для стиля лофт или хай-тек.', id: 3, cardAN: "#card-a-3"},
+    {src: './assets/images/furniture/furniture-1.webp', text: 'Современный трехместный диван с прямыми линиями. Имеет низкую спинку и тонкие высокие ножки, что придает конструкции легкость. Выполнен в лаконичном скандинавском стиле.', id: 1, cardAN: "#card-a-1", price: 1200},
+    {src: './assets/images/furniture/furniture-2.webp', text: 'Уютный диван песочного или светло-коричневого оттенка. Отличается пышными формами, мягкими подлокотниками и дополнительными подушками спинки. Идеально подойдет для традиционного интерьера.', id: 2, cardAN: "#card-a-2", price: 1400},
+    {src: './assets/images/furniture/furniture-3.webp', text: 'Стильный диван графитового цвета на металлическом каркасе. Дизайн подчеркнут четкими формами подушек и видимым основанием, что характерно для стиля лофт или хай-тек.', id: 3, cardAN: "#card-a-3", price: 1650},
     // beds
-    {src: './assets/images/furniture/furniture-4.webp', text: 'Минималистичная модель в натуральном светлом оттенке дерева. Имеет простое низкое изголовье и тонкие ножки. Застелена светлым постельным бельем с серым пледом в ногах.', id: 4, cardAN: "#card-a-4"},
-    {src: './assets/images/furniture/furniture-5.webp', text: 'Элегантная модель темно-серого цвета. Главная особенность — высокое мягкое изголовье с небольшими боковыми «ушками» (крыльями). Выглядит массивно и комфортно.', id: 5, cardAN: "#card-a-5"},
-    {src: './assets/images/furniture/furniture-6.webp', text: 'Модель из дерева насыщенного коричневого цвета с выраженной текстурой. Имеет классическое прямоугольное изголовье средней высоты. Дизайн сочетает в себе уют и строгость.', id: 6, cardAN: "#card-a-6"},
+    {src: './assets/images/furniture/furniture-4.webp', text: 'Минималистичная модель в натуральном светлом оттенке дерева. Имеет простое низкое изголовье и тонкие ножки. Застелена светлым постельным бельем с серым пледом в ногах.', id: 4, cardAN: "#card-a-4", price: 2300},
+    {src: './assets/images/furniture/furniture-5.webp', text: 'Элегантная модель темно-серого цвета. Главная особенность — высокое мягкое изголовье с небольшими боковыми «ушками» (крыльями). Выглядит массивно и комфортно.', id: 5, cardAN: "#card-a-5", price: 1950},
+    {src: './assets/images/furniture/furniture-6.webp', text: 'Модель из дерева насыщенного коричневого цвета с выраженной текстурой. Имеет классическое прямоугольное изголовье средней высоты. Дизайн сочетает в себе уют и строгость.', id: 6, cardAN: "#card-a-6", price: 2679},
     // shelves
-    {src: './assets/images/furniture/furniture-7.webp', text: 'Минималистичная композиция из четырех отдельных деревянных полок светлого оттенка. Они крепятся к стене без видимых кронштейнов, создавая эффект легкости. Расположены каскадом друг над другом.', id: 7, cardAN: "#card-a-7"},
-    {src: './assets/images/furniture/furniture-8.webp', text: 'Две полки, объединенные общим дизайном. Деревянные поверхности дополнены строгими черными металлическими рамами-держателями прямоугольной формы. Отлично подойдут для интерьера в стиле лофт.', id: 8, cardAN: "#card-a-8"},
-    {src: './assets/images/furniture/furniture-9.webp', text: 'Дизайнерская трехъярусная конструкция. Темные деревянные полки пересекаются тонкой металлической вертикальной опорой, смещенной от центра. Асимметричный и современный вид.', id: 9, cardAN: "#card-a-9"},
+    {src: './assets/images/furniture/furniture-7.webp', text: 'Минималистичная композиция из четырех отдельных деревянных полок светлого оттенка. Они крепятся к стене без видимых кронштейнов, создавая эффект легкости. Расположены каскадом друг над другом.', id: 7, cardAN: "#card-a-7", price: 350},
+    {src: './assets/images/furniture/furniture-8.webp', text: 'Две полки, объединенные общим дизайном. Деревянные поверхности дополнены строгими черными металлическими рамами-держателями прямоугольной формы. Отлично подойдут для интерьера в стиле лофт.', id: 8, cardAN: "#card-a-8", price: 539},
+    {src: './assets/images/furniture/furniture-9.webp', text: 'Дизайнерская трехъярусная конструкция. Темные деревянные полки пересекаются тонкой металлической вертикальной опорой, смещенной от центра. Асимметричный и современный вид.', id: 9, cardAN: "#card-a-9", price: 399},
     // closets
-    {src: './assets/images/furniture/furniture-10.webp', text: 'Высокий двухстворчатый шкаф в очень светлом (почти белом) дереве. Гладкие фасады без видимых ручек подчеркивают чистоту линий. Стоит практически вплотную к полу, скрывая ножки.', id: 10, cardAN: "#card-a-10"},
-    {src: './assets/images/furniture/furniture-11.webp', text: 'Компактный шкаф из дерева теплого коричневого оттенка с выраженной вертикальной текстурой. Имеет две дверцы с лаконичными ручками и установлен на тонкие, аккуратные ножки.', id: 11, cardAN: "#card-a-11"},
-    {src: './assets/images/furniture/furniture-12.webp', text: 'Двухдверный шкаф из темного дерева, приподнятый над полом на высоком и тонком черном металлическом каркасе. Такое основание делает массивную мебель визуально более легкой.', id: 12, cardAN: "#card-a-12"},
+    {src: './assets/images/furniture/furniture-10.webp', text: 'Высокий двухстворчатый шкаф в очень светлом (почти белом) дереве. Гладкие фасады без видимых ручек подчеркивают чистоту линий. Стоит практически вплотную к полу, скрывая ножки.', id: 10, cardAN: "#card-a-10", price: 979},
+    {src: './assets/images/furniture/furniture-11.webp', text: 'Компактный шкаф из дерева теплого коричневого оттенка с выраженной вертикальной текстурой. Имеет две дверцы с лаконичными ручками и установлен на тонкие, аккуратные ножки.', id: 11, cardAN: "#card-a-11", price: 1479},
+    {src: './assets/images/furniture/furniture-12.webp', text: 'Двухдверный шкаф из темного дерева, приподнятый над полом на высоком и тонком черном металлическом каркасе. Такое основание делает массивную мебель визуально более легкой.', id: 12, cardAN: "#card-a-12", price: 1290},
     // tables
-    {src: './assets/images/furniture/furniture-13.webp', text: 'Конструкция выполнена из светлого дерева в лаконичном скандинавском стиле. Столешница и массивные боковые опоры образуют единый монолитный контур. Отлично подойдет как для работы, так и в качестве обеденного стола в светлом интерьере.', id: 13, cardAN: "#card-a-13"},
-    {src: './assets/images/furniture/furniture-14.webp', text: 'Классическое сочетание массивной столешницы из дерева среднего тона и черного металлического подстолья. П-образные металлические ножки обеспечивают высокую устойчивость и придают мебели индустриальный характер.', id: 14, cardAN: "#card-a-14"},
-    {src: './assets/images/furniture/furniture-15.webp', text: 'Модель с выразительной столешницей из темного дерева с глубокой текстурой. Основание представляет собой тонкую, но прочную металлическую раму черного цвета. Выглядит элегантно и менее массивно, чем центральная модель, благодаря более тонким линиям каркаса.', id: 15, cardAN: "#card-a-15"},
+    {src: './assets/images/furniture/furniture-13.webp', text: 'Конструкция выполнена из светлого дерева в лаконичном скандинавском стиле. Столешница и массивные боковые опоры образуют единый монолитный контур. Отлично подойдет как для работы, так и в качестве обеденного стола в светлом интерьере.', id: 13, cardAN: "#card-a-13", price: 1100},
+    {src: './assets/images/furniture/furniture-14.webp', text: 'Классическое сочетание массивной столешницы из дерева среднего тона и черного металлического подстолья. П-образные металлические ножки обеспечивают высокую устойчивость и придают мебели индустриальный характер.', id: 14, cardAN: "#card-a-14", price: 1299},
+    {src: './assets/images/furniture/furniture-15.webp', text: 'Модель с выразительной столешницей из темного дерева с глубокой текстурой. Основание представляет собой тонкую, но прочную металлическую раму черного цвета. Выглядит элегантно и менее массивно, чем центральная модель, благодаря более тонким линиям каркаса.', id: 15, cardAN: "#card-a-15", price: 1179},
 ]
 
 // show all current offers
 
 currentGoods.innerText = contentCard.length;
 
+// make short text
+
+const futureShortedTexts = [];
+for (let j = 0; j < contentCard.length; j++) {
+    futureShortedTexts.push(contentCard[j].text);
+}
+
 // adding cards(dynamic adding)
 
 for (let i = 0; i < contentCard.length; i++) {
+    let shorted = shortText(futureShortedTexts)
     cardOfferImg[i].src = contentCard[i].src;
-    descriptionOfOffer[i].textContent = contentCard[i].text;
+    descriptionOfOffer[i].textContent = shorted[i];
 }
+
+// logic of cards button
+
+let getIdOfBtn;
+
+divOffers.addEventListener('click', (e) => {
+    const find = e.target.closest('.find');
+
+    if (find) {
+        getIdOfBtn = Number(find.dataset.offer);
+        modalOffer.showModal();
+        contentImageOffer.src = contentCard[getIdOfBtn - 1].src;
+        nameDescriptionOfOffer.textContent = contentCard[getIdOfBtn - 1].text;
+        price.textContent = `${contentCard[getIdOfBtn - 1].price}£`;
+        toShortForWhishlist.push(contentCard[getIdOfBtn - 1].text);
+    }
+})
 
 // logic of input search
 
@@ -185,20 +218,10 @@ function showResults(query) {
     else if (userSearchResult.length === 0) suggestedOffersByInput.classList.remove('shown');
     else {
         suggestedOffersByInput.classList.add('shown');
+        let shorted = shortText(userSearchResult)
         for (let i = 0; i < userSearchResult.length; i++) {
-            if (userSearchResult[i].length > 20) {
-                let splited = userSearchResult[i].split("");
-                let returnToStr = [];
-                for (let j = 0; j < 35; j++) {
-                    returnToStr.push(splited[j])
-                }
-                let restoredStr = returnToStr.join("").trim();
-                restoredStr += '...';
-                
-                suggestedOffersByInput.innerHTML += `<a href="${findIdResultFoundByUser[i]}">${restoredStr}</a><hr>`;
-            }
+            suggestedOffersByInput.innerHTML += `<a href="${findIdResultFoundByUser[i]}">${shorted[i]}</a><hr>`;
         }
-
         savedValue = undefined;
     }
 }
@@ -230,6 +253,31 @@ btnCatalog.addEventListener('mouseleave', () => {
 
 currentBag.addEventListener('click', () => {
     bagOfOffers.showModal();
+    
+})
+
+let toShortForWhishlist = [];
+const HTMLCollectionOfWishlist = []
+let indexRemoveFromWhislist = 0;
+throwInBin.addEventListener('click', () => {
+    indexRemoveFromWhislist++;
+    let shorted = shortText(toShortForWhishlist)
+    HTMLCollectionOfWishlist.push(`<div class="card-suggested-offer"><img class="img-suggested-offer" src="${contentCard[getIdOfBtn - 1].src}" alt="Мебель" width="70px" height="70px"><p class="p-suggested-offer">${shorted}</p><div class="div-suggested-offer"><button class="btn-suggested-offer" data-removeFromWhislist="${indexRemoveFromWhislist}"><img src="./assets/icons/bin.svg" alt="Отменить и убрать" width="50px" height="50px"></button></div></div>`);
+    for (let i = 0; i < HTMLCollectionOfWishlist.length; i++) {
+        suggestedOffers.innerHTML += HTMLCollectionOfWishlist[i];
+        i = HTMLCollectionOfWishlist.length - 1;
+    }
+    modalOffer.close();
+})
+
+suggestedOffers.addEventListener('click', (e) => {
+    const btnSuggestedOffer = e.target.closest('.btn-suggested-offer');;
+
+    if (btnSuggestedOffer) {
+        const idOfRemoveBtn = Number(btnSuggestedOffer.dataset.removefromwhislist);
+        console.log(idOfRemoveBtn);
+        
+    }
 })
 
 // close current dialog by cross button
@@ -256,3 +304,19 @@ order.addEventListener('click', () => {
         ifOrderSuccessful.classList.remove('shown');
     }, 9000);
 })
+
+function shortText(text) {
+    let restoredStr;
+    let restoredAll = [];
+    for (let i = 0; i < text.length; i++) {
+        let splited = text[i].split("");
+        let returnToStr = [];
+        for (let j = 0; j < 35; j++) {
+            returnToStr.push(splited[j])
+        }
+        restoredStr = returnToStr.join("").trim();
+        restoredStr += '...';
+        restoredAll.push(restoredStr);
+    }
+    return restoredAll;
+}
